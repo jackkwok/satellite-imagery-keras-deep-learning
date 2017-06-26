636ae3af19a7f24ad745c22fd04f86d91cf48209
Jack Kwok <ckwok@alum.mit.edu>
2017-06-25 19:58:28 -0700

    ensemble gives at least +0.002 improvement in LB F2 score

---
+++ utils/ensemble.py
@@ -0,0 +1,48 @@
+import numpy as np
+import numpy.ma as ma
+import pandas as pd
+import os
+
+from utils.predictor import submission_dataframe
+
+submission_dir = 'D:/Downloads/amazon/my_submissions/'
+ensemble_output_dir = submission_dir + 'ensemble/'
+
+submission_files = [
+	# WARNING!!!: ONLY USE FILES generated AFTER 6/25/2017 at 7pm (post Kaggle test data patch).
+	'submission_good_test_data_score_090909.csv',
+]
+
+def generate_ensemble_submission(ensemble_submission_filename):
+	""" generate a submission file based on majority vote amongst the submission files. 
+	Ensembling gives at least a +0.002 improvement in leaderboard score. """
+	class_names = ['slash_burn', 'clear', 'blooming', 'primary', 'cloudy', 
+		'conventional_mine', 'water', 'haze', 'cultivation', 'partly_cloudy', 
+		'artisinal_mine', 'habitation', 'bare_ground', 'blow_down', 
+		'agriculture', 'road', 'selective_logging']
+	num_classes = len(class_names)
+
+	num = len(submission_files)
+	for n in range(0, num):
+		df = pd.read_csv(submission_dir + submission_files[n])
+		for c in class_names:
+			df[c] = df['tags'].apply(lambda x: 1 if c in x.split(' ') else 0)
+
+		if n == 0:
+			names  = df.iloc[:, 0].values
+			N = df.shape[0]
+			predictions = np.zeros((N, num_classes), dtype=np.float32)
+
+		l = df.iloc[:,2:].values.astype(np.float32)
+		predictions = predictions+l
+	# average over all submission files
+	predictions = predictions/num
+
+	binary_predictions = (np.array(predictions) >= 0.5).astype(int)
+	predict_df = pd.DataFrame(binary_predictions, columns = class_names)
+	df_submission = pd.read_csv(submission_dir + submission_files[0])
+	submit_df = submission_dataframe(df_submission, predict_df)
+
+	ensemble_submission_filepath = ensemble_output_dir + ensemble_submission_filename
+	submit_df.to_csv(ensemble_submission_filepath, index=False)
+	print('submission file generated: {}'.format(ensemble_submission_filepath))
